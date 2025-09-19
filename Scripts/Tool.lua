5a52ca7bbc16eccd6a2f86b577ea12a918be347d
Unknown13557 <155928275+Unknown13557@users.noreply.github.com>
2025-09-19 11:42:18 +0700

    Update Tool.lua

--- Scripts/Tool.lua
+++ Scripts/Tool.lua
@@ -109,7 +109,8 @@ arc.Parent = icon
 -- ẢNH ICON: dùng placeholder mặc định Roblox
 -- (Không dùng ContentProvider:PreloadAsync nữa để tránh lỗi truyền string)
 icon.ImageTransparency = 1
-icon.Image = "rbxasset://textures/ui/PlayerList/Crown.png"    --
+icon.Image = "rbxasset://textures/ui/PlayerList/Roblox.png"
+
 icon.ImageTransparency = 0
 
 -- Drag icon (giữ đúng logic không double-toggle)
