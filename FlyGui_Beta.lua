da0558fa631bc883f2ee8b0770b8cf639fb3c841
Unknown13557 <155928275+Unknown13557@users.noreply.github.com>
2026-02-02 10:44:55 +0700

    Update FlyGui_Beta.lua

--- FlyGui_Beta.lua
+++ FlyGui_Beta.lua
@@ -222,7 +222,7 @@ end
 		local content = Instance.new("Frame")
 		content.Parent = slot
 		content.BackgroundTransparency = 1
-		content.Size = UDim2.new(1, -1, 1, 0)
+		content.Size = UDim2.new(1, 20, 1, 0)
 		content.Position = UDim2.fromOffset(10, 0)
 		content.ZIndex = 11
 
