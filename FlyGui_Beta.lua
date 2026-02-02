9878730f5ce973497cf70d87b3995e0226a3d8e8
Unknown13557 <155928275+Unknown13557@users.noreply.github.com>
2026-02-02 11:51:43 +0700

    Update FlyGui_Beta.lua

--- FlyGui_Beta.lua
+++ FlyGui_Beta.lua
@@ -288,11 +288,7 @@ layout.VerticalAlignment = Enum.VerticalAlignment.Center
 layout.HorizontalAlignment = Enum.HorizontalAlignment.Left
 layout.Padding = UDim.new(0, 2)
 
-local spWrap = Instance.new("Frame")
-spWrap.Parent = row
-spWrap.BackgroundTransparency = 1
-spWrap.Size = UDim2.fromOffset(110, 28)
-spWrap.AutomaticSize = Enum.AutomaticSize.None
+
 
 local yBox = Instance.new("TextBox")
 yBox.Parent = row
@@ -309,6 +305,15 @@ Instance.new("UICorner", yBox).CornerRadius = UDim.new(0,6)
 
 yBox.TextWrapped = false
 yBox.TextXAlignment = Enum.TextXAlignment.Center
+yBox.LayoutOrder = 1
+
+
+local spWrap = Instance.new("Frame")
+spWrap.Parent = row
+spWrap.BackgroundTransparency = 1
+spWrap.Size = UDim2.fromOffset(50, 28)
+spWrap.AutomaticSize = Enum.AutomaticSize.None
+spWrap.LayoutOrder = 2
 
 
 local spBox = Instance.new("TextBox")
